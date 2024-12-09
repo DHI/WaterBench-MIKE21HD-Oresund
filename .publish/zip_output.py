7276fb6fbf9d0a5620b2dcaef4cf7758f80ca5cd
FrejaTerpPetersen <frtp@dhigroup.com>
2024-12-09 16:09:37 +0100

    correct folder structure inside output-zipfile

--- .publish/zip_output.py
+++ .publish/zip_output.py
@@ -8,9 +8,9 @@ zip_fn = ".publish/MIKE21HD-Oresund-output.zip"
 def zip_repo():
     with zipfile.ZipFile(zip_fn, "w") as z:
         path_output = "//DKCPH1-STOR.DHI.DK/Projects/11560605/BenchmarkCases/WaterBench-MIKE21HD-Oresund/MIKE21HD-Oresund-output"
-        z.write(path_output + "/Area.dfsu")
-        z.write(path_output + "/Points.dfs0")
-        z.write("README_output.md")
+        z.write(path_output + "/Area.dfsu",arcname="Area.dfsu")
+        z.write(path_output + "/Points.dfs0",arcname="Points.dfs0")
+        z.write(".publish/README_output.md",arcname="README.md")
     print(f"Zip file created: {zip_fn}")
 
 
