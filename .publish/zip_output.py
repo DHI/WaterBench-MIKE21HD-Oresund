30b361dce140f8ab2cacd5161dee0a3f073a537d
FrejaTerpPetersen <frtp@dhigroup.com>
2025-02-26 12:07:31 +0100

    Updated path to datastore

--- .publish/zip_output.py
+++ .publish/zip_output.py
@@ -7,7 +7,7 @@ zip_fn = ".publish/MIKE21HD-Oresund-output.zip"
 
 def zip_repo():
     with zipfile.ZipFile(zip_fn, "w") as z:
-        path_output = "//DKCPH1-STOR.DHI.DK/Projects/11560605/BenchmarkCases/WaterBench-MIKE21HD-Oresund/MIKE21HD-Oresund-output"
+        path_output = "//DKCPH1-STOR.DHI.DK/Projects/11561006/WaterBench/WaterBench-MIKE21HD-Oresund/MIKE21HD-Oresund-output"
         z.write(path_output + "/Area.dfsu",arcname="Area.dfsu")
         z.write(path_output + "/Points.dfs0",arcname="Points.dfs0")
         z.write(".publish/README_output.md",arcname="README.md")
